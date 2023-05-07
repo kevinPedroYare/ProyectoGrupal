15a49b7cb8ba505bfa8ec92ad250b932908519a0
unknown <kyare@unsa.edu.pe>
2023-05-07 10:26:56 -0500

    Syntax de java

---
+++ Java_Syntax.java
@@ -0,0 +1,6 @@
+public class Java_Syntax{//esto es una clase
+    public static void main(String[] args){//esto es un metodo
+		System.out.println("Hola mundo");
+		System.out.println("La sintaxis es sencilla");
+	}
+}
