0ba3240c5a5d5ddaae7d8f4a2364c1b0f0136803
unknown <kyare@unsa.edu.pe>
2023-05-07 10:33:07 -0500

    Comentarios en Java

---
+++ Java_Comentarios.java
@@ -0,0 +1,11 @@
+public class Java_Comentarios{
+    public static void main(String[] args) {
+        //Esto es un comentario de una linea
+        /*
+         * Esto es un comentario de varias lineas
+         * Este es el segundo renglon
+         * Este es el tercer renglon
+         */
+        System.out.println("Comentarios en Java");
+    }
+}
