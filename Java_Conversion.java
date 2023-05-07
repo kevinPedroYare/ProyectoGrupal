a22c6b3caddb05b73d12994603b320a53926a1c2
unknown <kyare@unsa.edu.pe>
2023-05-07 10:43:53 -0500

    Conversion en Java

---
+++ Java_Conversion.java
@@ -0,0 +1,9 @@
+public class Java_Conversion{
+    public static void main(String[] args) {
+        int natural = 20;
+        double entero = natural;
+
+        System.out.println(natural);
+        System.out.println(entero);
+    }
+}
